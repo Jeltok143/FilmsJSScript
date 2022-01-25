fe359c4b78413690ea30b35312fd70c67bb22c3c
Jeltok <sinkvane@gmail.com>
2022-01-25 10:08:47 +0700

    second stage

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
 
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?');
+const b = prompt('На сколько ты его оценишь по 10 бальной шкале?');
+const c = prompt('Один из последних просмотренных фильмов?');
+const d = prompt('На сколько ты его оценишь по 10 бальной шкале?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
