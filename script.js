468232c274c44cd62fdccafa60a5c341c093e345
Jeltok <sinkvane@gmail.com>
2022-01-25 21:32:20 +0700

    loops and conditions added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,12 +10,28 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?');
-const b = prompt('На сколько ты его оценишь по 10 бальной шкале?');
-const c = prompt('Один из последних просмотренных фильмов?');
-const d = prompt('На сколько ты его оценишь по 10 бальной шкале?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?');
+  const b = prompt('На сколько ты его оценишь по 10 бальной шкале?');
+
+  if(a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if(personalMovieDB.count < 10) {
+  console.log('Мало фильмов ты посмотрел');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Ты классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Ты киноман!');
+} else {
+  console.log('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
